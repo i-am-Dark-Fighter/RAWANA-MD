d4203687289e0335f79249b99d422dccb720a47c
i-am-Dark-Fighter <dinujayagagana@gmail.com>
2025-06-21 23:40:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "wuUkCSyR#mZ2tRtvR7uqt6kG4iH20Vn2H6tSaq03wEkODRxRXFS0":process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined? "cz81XDgb#xOfOBKpNWSQd6GELYC7CmBva8327UuazNpQKOJPQtWo":process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "mongodb://mongo:fOWciohPEXvfhgujYmvidEtIJTrbGRRX@ballast.proxy.rlwy.net:41342",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
